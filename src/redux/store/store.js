50de5cbdcbb3bb8a81192d67f03e58d90afc12f9
ezelujan2 <eze.rosario2000@gmail.com>
2023-08-23 13:59:31 -0300

    Deploy

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,11 @@
-import {createStore, applyMiddleware,} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducer/reducer';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';  // Importa la función adecuada
 
 const store = createStore(
     reducer,
-    composeWithDevTools((applyMiddleware(thunk))),
-)
+    composeWithDevTools(applyMiddleware(thunk))  // Usa composeWithDevTools directamente
+);
 
 export default store;
